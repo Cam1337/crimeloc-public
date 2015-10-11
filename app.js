55e25f55bc4a9ad045171d043203ffb3701e2c22
Sam Doyle <sjd27@duke.edu>
2015-10-11 16:29:14 -0400

    added app.js for server code, iindex.html for client side, update readme for how to operate node locally.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
