36d5def96b5511fb7d8b41f9a19ed49ec437444d
eliwilliams <erw19@duke.edu>
2015-12-05 18:37:28 -0500

    Designed basic schema to parse JSON into SQL query. Todo: implement sanitization for robustness.

--- webapp/models/jsonToSQL.js
+++ webapp/models/jsonToSQL.js
@@ -3,5 +3,22 @@
  */
 
 module.exports.convert = function(jsobj){
-    return "SELECT Crime.Disposition, Crime.Type, Crime.Date, Crime.Time, Crime.Area_Name, Building.Lat, Building.Lon FROM Crime, Building WHERE Building.Name==Crime.Area_Name LIMIT 25";
+    
+    var prefix = "SELECT Crime.Disposition, Crime.Type, Crime.Date, Crime.Time, Crime.Area_Name, Building.Lat, Building.Lon FROM Crime, Building WHERE Building.Name==Crime.Area_Name";
+    var data = "";
+    var suffix = " LIMIT 50";
+    
+    // dummy json object for testing
+    var j = {
+        s_time: "00:00", // 24 hour (string)
+        s_date: "mm-dd-yyyy", // (string)
+        e_time: "00:00", // 24 hour (string)
+        e_date: "mm-dd-yyyy", // (string)
+        tags: [], // list of strings
+        desc: "Robbery", // string
+        loc: "Wilson" // string
+    }
+    
+    return prefix + data + suffix;
+    
 }
