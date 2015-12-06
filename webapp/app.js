dc0001a4be4cebece016ac16aeea76de6252e76f
Cameron Wrigley <cam@chicago.com>
2015-12-06 15:51:04 -0500

    changing null to 'null' for locations and tags

--- webapp/app.js
+++ webapp/app.js
@@ -23,8 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(mysql_orm)
-
 
 app.use('/', routes);
 
--- webapp/public/javascripts/client/multiSelectionLoader.js
+++ webapp/public/javascripts/client/multiSelectionLoader.js
@@ -1,3 +1,27 @@
-/**
- * Created by cam on 12/6/2015.
- */
+function createMultiselect(e_id){
+    $(e_id + " option:first").prop("selected",true).val("null");
+    $(e_id).multiselect({
+        onChange: function(option, checked, select){
+            $(e_id + " option:first").prop("selected",false);
+        }
+    });
+}
+
+function remoteLoadLocations(){
+    var location_select_opts = {
+        url: "/api/locations",
+        type: "get",
+        format: "html",
+        methods: {
+            before: function(el){
+                return {
+                    table: "Building",
+                    column: "Name"
+                }
+            }
+        }
+    };
+    remoteRequest("#location_selection_select", location_select_opts, function(){
+        createMultiselect("#location_selection_select");
+    });
+}
