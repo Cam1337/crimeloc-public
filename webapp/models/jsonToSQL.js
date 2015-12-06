03665ec925eacf29edaf073f85554926a6e2eb01
eliwilliams <erw19@duke.edu>
2015-12-06 17:34:44 -0500

    Queries work

--- webapp/models/jsonToSQL.js
+++ webapp/models/jsonToSQL.js
@@ -6,20 +6,87 @@ module.exports.convert = function(jsobj){
     console.log(jsobj);
     
     var prefix = "SELECT Crime.Disposition, Crime.Type, Crime.Date, Crime.Time, Crime.Area_Name, Building.Lat, Building.Lon FROM Crime, Building WHERE Building.Name==Crime.Area_Name";
-    var data = "";
-    var suffix = " LIMIT 50";
+    var suffix = " LIMIT 150";
     
-    // dummy json object for testing
-    var j = {
-        s_time: "00:00", // 24 hour (string)
-        s_date: "mm-dd-yyyy", // (string)
-        e_time: "00:00", // 24 hour (string)
-        e_date: "mm-dd-yyyy", // (string)
-        tags: [], // list of strings
-        desc: "Robbery", // string
-        loc: "Wilson" // string
-    }
+    var locs = convertToWhereIn("Crime.Area_Name", jsobj.building_name);
+    var tags = convertToWhereIn("Crime.Type", jsobj.crime_type);
+    var types = convertToWhereIn("Building.Type", jsobj.building_type);
+    var date = convertDateTime("Crime.Date", jsobj.lower_date, jsobj.upper_date);
+    var time = convertDateTime("Crime.Time", jsobj.lower_time, jsobj.upper_time);
     
+    var data = conjunctStatements(locs, tags, types, date, time);
+    
+    console.log(prefix + data + suffix);
     return prefix + data + suffix;
     
+}
+
+
+function convertToWhereIn(column, list) {
+    
+    var statement = "";
+    
+    if(list === "null") {
+         return statement;
+    }
+    
+    statement += column + " IN (";
+    
+    for(var i = 0; i < list.length; i++) {
+        statement += "'" + list[i] + "'";
+        if(i != list.length - 1) {
+           statement += ",";
+        }
+    }
+    
+    statement += ")";
+    
+    return statement;
+}
+
+
+function convertDateTime(column, start, end) {
+    
+    var statement = "";
+    
+    if(start !== "null") {
+        statement += column + " > " + "'" + start + "'";
+    }
+    
+    if(end !== "null") {
+        if(start !== "null") {
+            statement += " AND ";
+        }
+        statement += column + " < " + "'" + end + "'";
+    }
+    
+    return statement;
+}
+
+
+function conjunctStatements(location, tag, types, date, time) {
+    
+    var statement = "";
+    
+    if(location !== "") {
+        statement += " AND " + location;
+    }
+    
+    if(tag !== "") {
+        statement += " AND " + tag;
+    }
+    
+    if(types !== "") {
+        statement += " AND " + types;
+    }
+    
+    if(date !== "") {
+        statement += " AND " + date;
+    }
+    
+    if(time !== "") {
+        statement += " AND " + time;
+    }
+    
+    return statement;
 }
